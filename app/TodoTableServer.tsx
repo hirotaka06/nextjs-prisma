07d92d9ee9bbf150efae6d2f8140b81b8a2f0860
hirotaka06 <h.kojima572@gmail.com>
2025-01-02 19:16:25 +0900

    　

--- app/TodoTableServer.tsx
+++ app/TodoTableServer.tsx
@@ -3,9 +3,12 @@ import TodotableClient from "./TodotableClient";
 import { Todo } from "@/app/types";
 
 async function fetchTodos(): Promise<Todo[]> {
-  const response = await fetch("http://localhost:3000/api/todos", {
-    cache: "no-store",
-  });
+  const response = await fetch(
+    "https://nextjs-prisma-phi.vercel.app/api/todos",
+    {
+      cache: "no-store",
+    }
+  );
   if (!response.ok) {
     throw new Error("Failed to fetch todos");
   }
