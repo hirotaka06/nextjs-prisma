d6f3f43cb71f465c5a3ef790654619a003dda9c9
hirotaka06 <h.kojima572@gmail.com>
2025-01-02 19:29:56 +0900

    　

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
       <Header />
       <div className="container px-4 md:px-6 max-w-4xl mx-auto pt-8">
         <div className="flex flex-col gap-4 mb-8 font-mono">
-          <h1 className="text-4xl md:text-5xl font-bold ">User Directory</h1>
+          <h1 className="text-4xl md:text-5xl font-bold ">
+            ToDo App with Slack
+          </h1>
           <p className="text-lg text-muted-foreground">
             My first project using a RESTful API
           </p>
