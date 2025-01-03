b7555be8e481f2f1ce849dd132f8a13f6cec1b8c
hirotaka06 <h.kojima572@gmail.com>
2025-01-03 13:10:54 +0900

    　

--- app/api/[id]/route.ts
+++ app/api/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import prisma from "@/lib/PrismaClient";
+import { prisma } from "@/lib/PrismaClient";
 
 export async function DELETE(
   request: Request,
